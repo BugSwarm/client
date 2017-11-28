1f6b23d77114f300af8262e86698c8618c3e7111
Naji Dmeiri <ndmeiri@users.noreply.github.com>
2017-11-28 15:05:44 -0800

    Add support for --sandbox argument
    Remove support for --shared-dir argument

--- bugswarm.py
+++ bugswarm.py
@@ -1,5 +1,4 @@
 import logging
-import os
 import pprint
 
 import click
@@ -18,30 +17,13 @@ def cli():
     log.info('Note that Docker requires sudo.')
 
 
-def _validate_shared_dir(ctx, param, value):
-    if value is None:
-        return value
-
-    host_dir, container_dir = value.split(':', 1)
-
-    # Since we do not know the the filesystem of the container, we cannot validate container_dir. We cannot even check
-    # if it is an absolute path because that would require that the container is the same platform as the host, which is
-    # too strong of an assumption. Instead, we pass validation responsibility to docker; it will complain if necessary.
-    #
-    # However, we can check that host_dir is an absolute path to an existing directory on the host machine.
-    if not host_dir or not os.path.isdir(host_dir) or not container_dir:
-        raise click.BadParameter('The volume binding must be a colon-separated pair of absolute paths to a directory '
-                                 'on the host and the container, respectively. '
-                                 '(e.g. /home/username/bugswarm-sandbox:/home)')
-    return host_dir, container_dir
-
-
 @cli.command()
 @click.option('--image-tag', required=True, type=str)
-@click.option('--script', type=click.Path(file_okay=True, path_type=str))
-@click.option('--shared-dir', callback=_validate_shared_dir, default=None, type=str)
-def run(image_tag, script, shared_dir):
-    docker.docker_run(image_tag, script, shared_dir)
+@click.option('--script', type=click.Path(exists=False, file_okay=True, dir_okay=False, path_type=str))
+@click.option('--sandbox',
+              type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True, path_type=str))
+def run(image_tag, script, sandbox):
+    docker.docker_run(image_tag, script, sandbox)
 
 
 @cli.command()
--- client/docker.py
+++ client/docker.py
@@ -1,52 +1,72 @@
 import subprocess
 
+import click
+
 from bugswarmcommon import log
 from bugswarmcommon.credentials import DOCKER_HUB_REPO
+
+SCRIPT_DEFAULT = '/bin/bash'
+CONTAINER_SANDBOX_DEFAULT = '/bugswarm-sandbox'
 
 
 # By default, this function downloads the image, enters the container, and executes '/bin/bash' in the container.
 # The executed script can be changed by passing the script argument.
-def docker_run(image_tag, script=None, shared_dir=None):
+def docker_run(image_tag, script=None, sandbox=None):
     assert isinstance(image_tag, str) and not image_tag.isspace()
 
-    SCRIPT_DEFAULT = '/bin/bash'
     script = script or SCRIPT_DEFAULT
     using_script = script != SCRIPT_DEFAULT
     assert isinstance(script, str) and not script.isspace()
 
-    host_dir, container_dir = None, None
-    using_shared_dir = False
-    if shared_dir is not None:
-        assert isinstance(shared_dir, tuple)
-        assert len(shared_dir) == 2
-        host_dir, container_dir = shared_dir
-        assert isinstance(host_dir, str) and not host_dir.isspace()
-        assert isinstance(container_dir, str) and not container_dir.isspace()
-        using_shared_dir = True
+    using_sandbox = False
+    if sandbox is not None:
+        assert isinstance(sandbox, str) and not sandbox.isspace()
+        using_sandbox = True
 
     # First, try to pull the image.
     ok = docker_pull(image_tag)
     if not ok:
         return False
 
-    # Communicate what is happening next to the user.
-    if using_shared_dir:
-        log.info('Binding host directory', host_dir, 'to container directory', container_dir)
+    # Communicate progress to the user.
+    if using_sandbox:
+        # Confirm that the user wants to use the passed sandbox.
+        if not click.confirm('\n'
+                             'Due to a limitation of Docker, your sandbox directory ({}) and everything it contains '
+                             'will become readable, writable, and executable by everyone.\n'
+                             'Are you sure you want to continue?'.format(sandbox)):
+            return False
 
+    # Communicate progress to the user.
+    container_sandbox = CONTAINER_SANDBOX_DEFAULT
+    if using_sandbox:
+        log.info('Binding host sandbox', sandbox, 'to container directory', container_sandbox)
+        # log.info('Binding host sandbox to container directory:',
+        #           sandbox, '(host) <-----> (container)', container_sandbox)
+
+    # Communicate progress to the user.
     if using_script:
         log.info('Entering the container and executing', script + '.')
     else:
         log.info('Entering the container.')
 
-    # Now try to run the image.
+    # Try to run the image.
     image_location = _image_location(image_tag)
-    volume_args = ['-v', ':'.join([host_dir, container_dir])] if shared_dir else []
+    volume_args = ['-v', '{}:{}'.format(sandbox, container_sandbox)] if using_sandbox else []
 
     # If we're using a shared directory, we need to modify the start script to change the permissions of the shared
     # directory on the container side. However, this will also change the permissions on the host side.
     script_args = [script]
-    if using_shared_dir:
-        script_args = [SCRIPT_DEFAULT, '-c', 'sudo chmod 777 {} && {}'.format(container_dir, script)]
+    if using_sandbox:
+        start_command = 'sudo chmod -R 777 {} && cd {} && umask 000 && cd .. && {}'.format(
+            container_sandbox,
+            container_sandbox,
+            script)
+        # These arguments represent a command of the following form:
+        # /bin/bash -c "sudo chmod 777 <container_sandbox> && cd <container_sandbox> && umask 000 && /bin/bash"
+        # So bash will execute chmod and umask and then start a new bash shell. From the user's perspective, the chmod
+        # and umask commands happen transparently. That is, the user only sees the final new bash shell.
+        script_args = [SCRIPT_DEFAULT, '-c', start_command]
 
     args = ['sudo', 'docker', 'run', '--privileged'] + volume_args + ['-i', '-t', image_location] + script_args
     process = subprocess.Popen(args)
