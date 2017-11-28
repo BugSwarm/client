0335021e371f250988dcc2c25908f3495a2c33f6
Naji Dmeiri <ndmeiri@users.noreply.github.com>
2017-11-28 12:39:50 -0800

    Use chmod 777 for shared directory;  clean up logic and variable names for optional parameters

--- client/docker.py
+++ client/docker.py
@@ -11,28 +11,29 @@ def docker_run(image_tag, script=None, shared_dir=None):
 
     SCRIPT_DEFAULT = '/bin/bash'
     script = script or SCRIPT_DEFAULT
+    using_script = script != SCRIPT_DEFAULT
     assert isinstance(script, str) and not script.isspace()
 
     host_dir, container_dir = None, None
+    using_shared_dir = False
     if shared_dir is not None:
         assert isinstance(shared_dir, tuple)
         assert len(shared_dir) == 2
         host_dir, container_dir = shared_dir
         assert isinstance(host_dir, str) and not host_dir.isspace()
         assert isinstance(container_dir, str) and not container_dir.isspace()
+        using_shared_dir = True
 
     # First, try to pull the image.
     ok = docker_pull(image_tag)
     if not ok:
         return False
 
-    # Communicate what is happening next to the user. The output depends on the passed script and shared_dir parameters.
-    default_shared_dir = host_dir is None and container_dir is None
-    if not default_shared_dir:
+    # Communicate what is happening next to the user.
+    if using_shared_dir:
         log.info('Binding host directory', host_dir, 'to container directory', container_dir)
 
-    default_script = script == SCRIPT_DEFAULT
-    if not default_script:
+    if using_script:
         log.info('Entering the container and executing', script + '.')
     else:
         log.info('Entering the container.')
@@ -40,7 +41,14 @@ def docker_run(image_tag, script=None, shared_dir=None):
     # Now try to run the image.
     image_location = _image_location(image_tag)
     volume_args = ['-v', ':'.join([host_dir, container_dir])] if shared_dir else []
-    args = ['sudo', 'docker', 'run', '--privileged'] + volume_args + ['-i', '-t', image_location, script]
+
+    # If we're using a shared directory, we need to modify the start script to change the permissions of the shared
+    # directory on the container side. However, this will also change the permissions on the host side.
+    script_args = [script]
+    if using_shared_dir:
+        script_args = [SCRIPT_DEFAULT, '-c', 'sudo chmod 777 {} && {}'.format(container_dir, script)]
+
+    args = ['sudo', 'docker', 'run', '--privileged'] + volume_args + ['-i', '-t', image_location] + script_args
     process = subprocess.Popen(args)
     _ = process.communicate()
     return process.returncode == 0
