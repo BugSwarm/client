4992ebca00d79cf75a95fb961e6c52c153c8e029
Naji Dmeiri <ndmeiri@users.noreply.github.com>
2017-12-06 12:44:52 -0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -18,11 +18,15 @@ setup(
     url='https://github.com/BugSwarm/client',
     download_url='https://github.com/BugSwarm/client/archive/0.1.tar.gz',
     keywords=['bugswarm', 'client', 'cli', 'dataset', ],
-
     install_requires=[
         'Click==6.7',
         'requests==2.18.4',
+        'bugswarmcommon==0.1'
     ],
+    dependency_links=[
+        'git+https://github.com/BugSwarm/common.git#egg=bugswarmcommon-0.1',
+    ],
+
     entry_points='''
         [console_scripts]
         bugswarm=bugswarm:cli
