b7e4c57a44e553f0f452103aa2aba2684c3f2720
Naji Dmeiri <ndmeiri@users.noreply.github.com>
2018-04-23 13:15:23 -0700

    Update dependency versions

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=[
         'Click==6.7',
         'requests==2.18.4',
-        'bugswarm-common==0.0.1',
+        'bugswarm-common==0.0.2',
     ],
 
     entry_points={
