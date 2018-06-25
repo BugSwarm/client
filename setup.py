13088a3171888be1799bd08846e0d5bd62ed1ea9
Naji Dmeiri <ndmeiri@users.noreply.github.com>
2018-06-25 11:31:37 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='bugswarm-client',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/BugSwarm/client',
     author='BugSwarm',
     author_email='dev.bugswarm@gmail.com',
