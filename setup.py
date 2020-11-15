5f47d2d61fc101cf590983336158929f87791189
Eric Li <ercli@ucdavis.edu>
2020-11-15 13:28:45 -0800

    Increase version number to 0.1.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='bugswarm-client',
-    version='0.1.6',
+    version='0.1.7',
     url='https://github.com/BugSwarm/client',
     author='BugSwarm',
     author_email='dev.bugswarm@gmail.com',
